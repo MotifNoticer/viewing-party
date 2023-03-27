9ee3709ba76f91d4c3b9313066a572897eb1807c
Sydney Alberson <sydney.alberson@gmail.com>
2023-03-27 13:35:50 -0400

    updated add_to_watched function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,18 +1,23 @@
 # ------------- WAVE 1 --------------------
 
-def create_movie(title, genre, rating):
-    
+def create_movie(title, genre, rating):   
     new_movie = {}
     
     if not title or not genre or not rating:
         return None
-
-    else:
-        new_movie["title"] = title
-        new_movie["genre"] = genre
-        new_movie["rating"] = rating
+    
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
 
     return new_movie
+
+def add_to_watched(user_data, movie):
+
+    user_data["watched"] = [movie]
+
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -23,7 +28,7 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
