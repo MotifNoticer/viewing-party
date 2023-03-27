14f349027c1ade385163bb0f9ee278a444c80ed1
Sydney Alberson <sydney.alberson@gmail.com>
2023-03-27 12:00:44 -0400

    passed tests that return None with no title, genre, or rating

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,11 +1,16 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    
     new_movie = {}
+    
+    if not title or not genre or not rating:
+        return None
 
-    new_movie["title"] = title
-    new_movie["genre"] = genre
-    new_movie["rating"] = rating
+    else:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
 
     return new_movie
 
