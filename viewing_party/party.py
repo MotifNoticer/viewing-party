55cc6e61b83464ab0a8974dda453e09e8b0afd58
Kaliane <vankaliane@gmail.com>
2023-03-28 15:06:26 -0400

    party py merge test wave 3

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -85,58 +85,51 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+# USER_DATA_3 = copy.deepcopy(USER_DATA_2)
+# USER_DATA_3["friends"] =  [
+#         {
+#             "watched": [
+#                 FANTASY_1,
+#                 FANTASY_3,
+#                 FANTASY_4,
+#                 HORROR_1,
+#             ]
+#         },
+#         {
+#             "watched": [
+#                 FANTASY_1,
+#                 ACTION_1,
+#                 INTRIGUE_1,
+#                 INTRIGUE_3,
+#             ]
+#         }
+#     ]  
+
+# function to return list a list of dictionaries, that represents a list of movies
+        # that she has watched but her friends haven't
 def get_unique_watched(user_data):
 
-# Consider the movies that the user has watched, 
-# and consider the movies that their friends have watched. 
-# Determine which movies the user has watched, 
-# but none of their friends have watched.
-# Return a list of dictionaries, that represents a list of movies
-
-    user_watched = user_data["watched"]
-    friends = user_data["friends"]
-
-    user_unique_movies = []
-
-    friends_movie_watched = []
-
-    for friend in friends:
-        for i in range(len(friend["watched"])):
-            friends_movie_watched.append(friend["watched"][i]) 
-
-    for movie in user_watched:
-        if movie not in friends_movie_watched:
-            if movie not in user_unique_movies:
-                user_unique_movies.append(movie)
-
-    return user_unique_movies
-
-def get_friends_unique_watched(user_data):
+    unique_user_data = []
     
-# Consider the movies that the user has watched, 
-# and consider the movies that their friends have watched. 
-# Determine which movies at least one of the user's friends have watched, 
-# but the user has not watched.
-# Return a list of dictionaries, that represents a list of movies
-    
-    user_watched = user_data["watched"]
-    friends = user_data["friends"]
+    for unique_user_movie
 
-    friends_unique_movies = []
+    # iterate through friend list index range  
 
-    friends_movie_watched = []
+    if user_data["title"] not in user_data["friends"]["watched"]:
+        user_data += 1
+        
+    # if user_data["title"] in user_data["friends"]["watched"]:
+    #     user_data += 0
+    else:
+        user_data += 0
 
-    # Big O notation of O(n^2), try to refactor
-    for friend in friends:
-        for i in range(len(friend["watched"])):
-            friends_movie_watched.append(friend["watched"][i]) 
 
-    for movie in friends_movie_watched:
-        if movie not in user_watched:
-            if movie not in friends_unique_movies:
-                friends_unique_movies.append(movie)
 
-    return friends_unique_movies
+# function to return a list of dictionaries, that represents a list of movies
+        # that a friend has watched but Amanda hasen't
+# def get_friends_unique_watched(user_data):
+
+    unique_friends_data = []
 
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
