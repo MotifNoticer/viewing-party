e004091fa78490ab7d86a1985fab6bca1ca09af0
Sydney Alberson <sydney.alberson@gmail.com>
2023-03-28 15:02:20 -0400

    completed wave 03

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -86,10 +86,57 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 
 def get_unique_watched(user_data):
-    pass
+
+# Consider the movies that the user has watched, 
+# and consider the movies that their friends have watched. 
+# Determine which movies the user has watched, 
+# but none of their friends have watched.
+# Return a list of dictionaries, that represents a list of movies
+
+    user_watched = user_data["watched"]
+    friends = user_data["friends"]
+
+    user_unique_movies = []
+
+    friends_movie_watched = []
+
+    for friend in friends:
+        for i in range(len(friend["watched"])):
+            friends_movie_watched.append(friend["watched"][i]) 
+
+    for movie in user_watched:
+        if movie not in friends_movie_watched:
+            if movie not in user_unique_movies:
+                user_unique_movies.append(movie)
+
+    return user_unique_movies
 
 def get_friends_unique_watched(user_data):
-    pass
+    
+# Consider the movies that the user has watched, 
+# and consider the movies that their friends have watched. 
+# Determine which movies at least one of the user's friends have watched, 
+# but the user has not watched.
+# Return a list of dictionaries, that represents a list of movies
+    
+    user_watched = user_data["watched"]
+    friends = user_data["friends"]
+
+    friends_unique_movies = []
+
+    friends_movie_watched = []
+
+    # Big O notation of O(n^2), try to refactor
+    for friend in friends:
+        for i in range(len(friend["watched"])):
+            friends_movie_watched.append(friend["watched"][i]) 
+
+    for movie in friends_movie_watched:
+        if movie not in user_watched:
+            if movie not in friends_unique_movies:
+                friends_unique_movies.append(movie)
+
+    return friends_unique_movies
 
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
